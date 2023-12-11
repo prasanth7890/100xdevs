984aab5734f5656d1aaef4eb1c71a199361ad3fe
prasanth7890 <maryratnamp@gmail.com>
2023-12-11 17:47:01 +0530

    solved hard lvl assigment

--- Week-2/01-async-js/hard (promises)/3-promise-all.js
+++ Week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -5,19 +5,35 @@
  */
 
 function wait1(t) {
-
+    return new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            resolve(t);
+        }, (t * 1000));
+    });   
 }
 
 function wait2(t) {
-
+    return new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            resolve(t);
+        }, (t * 1000));
+    });   
 }
 
 function wait3(t) {
-
+    return new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            resolve(t);
+        }, (t * 1000));
+    });   
 }
 
 function calculateTime(t1, t2, t3) {
-
+    const start = Date.now();
+    return Promise.all([wait1(t1), wait2(t2), wait3(t3)]).then((result) => {
+        const end = Date.now();
+        return end-start;
+    })
 }
 
 module.exports = calculateTime;
--- Week-2/01-async-js/hard (promises)/4-promise-chain.js
+++ Week-2/01-async-js/hard (promises)/4-promise-chain.js
@@ -6,19 +6,43 @@
  */
 
 function wait1(t) {
-
+    return new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            resolve();
+        }, (t * 1000));
+    });  
 }
 
 function wait2(t) {
-
+    return new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            resolve();
+        }, (t * 1000));
+    });  
 }
 
 function wait3(t) {
-
+    return new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            resolve();
+        }, (t * 1000));
+    });  
 }
 
 function calculateTime(t1, t2, t3) {
-
+    const start = Date.now();
+    return new Promise(function (resolve) {
+        wait1(t1).then(function () {
+            wait2(t2).then(function () {
+              wait3(t3).then(function () {
+                  const end = Date.now();  
+                  resolve(end - start);
+              });
+            });
+          }); 
+    }).then(function (result) {
+        return result;
+    });
 }
 
 module.exports = calculateTime;
