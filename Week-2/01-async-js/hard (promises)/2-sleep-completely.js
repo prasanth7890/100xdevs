43c3a759a48e68244922b9731f09b6e11094f3de
prasanth7890 <maryratnamp@gmail.com>
2023-12-11 18:21:04 +0530

    added sync code for sleepfunction

--- Week-2/01-async-js/hard (promises)/2-sleep-completely.js
+++ Week-2/01-async-js/hard (promises)/2-sleep-completely.js
@@ -4,12 +4,28 @@
  * the function should return a promise just like before
  */
 
+
+// sync code
 function sleep(milliseconds) {
-    return new Promise(function(resolve, reject) {
-        setTimeout(() => {
-            resolve();
-        }, (milliseconds));
-    });   
-}
+    return new Promise(function(resolve) {
+        const start = Date.now();
+        let now = start;
+
+        while (now - start < milliseconds) {
+          now = Date.now();
+        }
+        resolve()
+    })
+  }
+
+
+// async code 
+// function sleep(milliseconds) {
+//     return new Promise(function(resolve, reject) {
+//         setTimeout(() => {
+//             resolve();
+//         }, (milliseconds));
+//     });   
+// }
 
 module.exports = sleep;
