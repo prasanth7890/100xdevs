271833ad80261f17724e73ab4d7d06b504a5d3a6
prasanth7890 <maryratnamp@gmail.com>
2024-01-18 19:42:48 +0530

    added styles

--- week-7/Question-5/github-infocard/src/App.jsx
+++ week-7/Question-5/github-infocard/src/App.jsx
@@ -1,45 +1,69 @@
-import './App.css';
+import { useState, useTransition } from "react";
+import "./App.css";
 
 function App() {
+  const [state, setState] = useState("");
+  const [user, setUser] = useState();
+
+  async function handleClick() {
+    const response = await fetch(`https://api.github.com/users/${state}`);
+    const userDetails = await response.json();
+    
+    if(userDetails) {
+      console.log(userDetails);
+    }
+  }
+
   return (
-    <div className='main'>
+    <div className="main">
       <div>
-        <input type="text" />
-        <button>Search</button>
+        <input
+          type="text"
+          value={state}
+          onChange={(e) => setState(e.target.value)}
+        />
+        <button onClick={handleClick}>Search</button>
       </div>
+
       <div className="profile-div">
-        <img src="" alt="image" />
         <div>
-          <div>name</div>
-          <div>
-            <span>username</span>
-            <a href="">URL</a>
-          </div>
+          <img
+            src="https://cdn5.vectorstock.com/i/1000x1000/65/54/cute-anime-girl-in-black-hoodie-and-green-eyes-vector-39706554.jpg"
+            alt="image"
+          />
+        </div>
+
+        <div>Prasanth</div>
+
+        <div className="username">
+          <span>prasanth7890</span>
+          <a href="">GITHUB</a>
         </div>
 
         <div>
-          <span>location: location</span>
+          <span className="location">From: location</span>
           <span>email: useremail</span>
         </div>
 
-        <div style={{display: "flex"}}>
-          <div>
-            <div>public repos</div>
-            <span>count</span>
+        <div className="stats">
+          <div className="inner">
+            <div>Repos</div>
+            <div>count</div>
           </div>
-          <div>
+
+          <div className="inner">
             <div>followers</div>
-            <span>count</span>
+            <div>count</div>
           </div>
-          <div>
+
+          <div className="inner">
             <div>following</div>
-            <span>count</span>
+            <div>count</div>
           </div>
         </div>
-
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
