206f7b720096d01fa1b34515269f443f40151be5
prasanth7890 <maryratnamp@gmail.com>
2024-01-19 16:48:22 +0530

    Solved Question-6

---
+++ week-7/Question-6/otp/src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+  const [render, setRender] = useState(false);
+
+  function handleOnclick() {
+    setRender(true);
+  }
+
+  function handleInput(target) {
+    const value = target.value;
+    // if entered char is not a number then, it gets replaced.
+    if(isNaN(value)) {
+      target.value = "";
+      return;
+    }
+
+    if(value != "") {
+      if(target.nextElementSibling) {
+        target.nextElementSibling.focus(); //shifts focus to next element.
+      }
+    }
+  }
+
+  function hanldeOnkeyup(e) {
+    const key = e.key;
+
+    if(key == "Backspace") {
+      e.target.value = "";
+      const previous = e.target.previousElementSibling;
+      if(previous) {
+        previous.focus();
+      }
+    }
+  }
+
+  return (
+    <>
+      <div className="main">
+        <div className="inner">
+          <h2>Login Via OTP</h2>
+          {render === false ? (
+            <>
+              <input type="text" placeholder="Enter your mobile number" />
+              <button onClick={handleOnclick}>send OTP</button>
+            </>
+          ) : (
+            <>
+              <div className="otp-div">
+                <input type="text" className="otp" onInput={(e)=>{handleInput(e.target)}} onKeyUp={(e)=>{hanldeOnkeyup(e)}}/>
+                <input type="text" className="otp" onInput={(e)=>{handleInput(e.target)}} onKeyUp={(e)=>{hanldeOnkeyup(e)}}/>
+                <input type="text" className="otp" onInput={(e)=>{handleInput(e.target)}} onKeyUp={(e)=>{hanldeOnkeyup(e)}}/>
+                <input type="text" className="otp" onInput={(e)=>{handleInput(e.target)}} onKeyUp={(e)=>{hanldeOnkeyup(e)}}/>
+              </div>
+              <button>Login</button>
+              <button onClick={()=>setRender(false)}>go back</button>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
