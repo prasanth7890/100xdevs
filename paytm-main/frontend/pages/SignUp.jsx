cc06096bc790c2780c32f86dbdb5d24c72abca60
prasanth7890 <maryratnamp@gmail.com>
2024-01-25 14:39:45 +0530

    completed signup ui

--- paytm-main/frontend/pages/SignUp.jsx
+++ paytm-main/frontend/pages/SignUp.jsx
@@ -1,11 +1,78 @@
-import React from 'react'
+import { useState } from "react";
+import axios from 'axios';
+import {useNavigate} from "react-router-dom";
+
 
 const SignUp = () => {
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSubmit() {
+
+    const UserData = {
+      "username": username,
+      "firstName": firstName,
+      "lastName": lastName,
+      "password": password
+    }
+
+    try {
+      const response = await axios.post('http://localhost:3000/api/v1/user/signup', UserData);
+      if(response.statusText === "OK") {
+        navigate('/dashboard');
+      }
+      
+    }catch(error) {
+      console.log(error);
+    }
+    
+  }
+
+
   return (
     <div>
-      SignUp
+      <div className="bg-white w-[310px] rounded-md absolute left-[40%] top-[15%] border-black border-[1px] pl-[5px] pr-[5px]" >
+        <div className='flex flex-col items-center pt-4'>
+          <h1 className='text-3xl font-bold pb-2'>Sign Up</h1>
+          <p className='text-center'>Enter your information to create an account</p>
+        </div>
+
+        <div className='flex flex-col items-center mt-2'>
+          <div>
+            <div className='font-bold mb-2'>First Name</div>
+            <input onChange={(e)=>setFirstName(e.target.value)} value={firstName} type="text" placeholder='John' className='w-[280px] h-9 rounded-md pl-3 border-slate-300 border-[1px]'/>
+          </div>
+
+          <div>
+            <div className='font-bold mb-2'>Last Name</div>
+            <input onChange={(e)=>setLastName(e.target.value)} value={lastName} type="text" placeholder='Doe'className='w-[280px] h-9 rounded-md pl-3 border-slate-300 border-[1px]'/>
+          </div>
+
+          <div>
+            <div className='font-bold mb-2'>Email</div>
+            <input onChange={(e)=>setUsername(e.target.value)} value={username} type="text" placeholder='johndoe@example.com'className='w-[280px] h-9 rounded-md pl-3 border-slate-300 border-[1px]'/>
+          </div>
+
+          <div>
+            <div className='font-bold mb-2'>Password</div>
+            <input type={'password'} onChange={(e)=>setPassword(e.target.value)} value={password} className='w-[280px] h-9 rounded-md pl-3 border-slate-300 border-[1px]'/>
+          </div>
+
+        <button onClick={handleSubmit} className='bg-black text-white w-[280px] h-9 rounded-md mt-3 pb-1 mb-3'>Sign Up</button>
+
+        <div className='mb-5'>Already have an account? <a href="" className='underline'>Login</a></div>
+
+        </div>
+
+
+
+      </div>
     </div>
   )
 }
 
-export default SignUp
+export default SignUp;
---
+++ paytm-main/frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./pages/**/*.{html,js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
